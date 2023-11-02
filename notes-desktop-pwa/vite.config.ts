330174b3e5783d9c1b5a5feb64ab510502a44dfe
Christopher Williams <williamstophef@gmail.com>
2023-11-01 22:32:05 -0700

    Update before running build

--- notes-desktop-pwa/vite.config.ts
+++ notes-desktop-pwa/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Notes PWA",
+        short_name: "NPWA",
+      },
+    }),
+    react(),
+  ],
+  build: {
+    outDir: "build",
+  },
+});
